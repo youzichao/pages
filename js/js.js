76d73ce33fd888bd36f73e5b51174324886b9cb4
Unknown <zichao_you@163.com>
2017-08-20 12:44:44 +0800

    改变js

--- js/js.js
+++ js/js.js
@@ -1,4 +1,15 @@
 (function(g){
-	for(var p in g)
-	document.writeln(p+'--------------------'+g[p]);
+	var changecolr = function xx(){
+		var r = getroamd();
+		var g = getroamd();
+		var b = getroamd();
+		document.body.style.backgroundColor = 'rgb('+r+','+g+','+b+')';
+		setTimeout(function(){
+			xx();
+		},500);
+	}
+	var getroamd = function(){
+		return Math.floor(Math.random() * 200);
+	}
+	changecolr();
 })(window);
